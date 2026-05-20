8ed90ecad3c93a35480d13d6a0b1ea08eb8988ab
Fatema <fatematz486@gmail.com>
2026-05-20 18:22:20 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+const cors = require("cors")
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+dotenv.config()
+
+const PORT = process.env.PORT
+const uri = process.env.MONGODB_URI;
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("ideaVault")
+    const addIdeaCollection = db.collection("addIdea")
+    const ideasCollection = db.collection("ideas")
+    const commentsCollection = db.collection('comments');
+
+
+
+
+
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Keep connection alive
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("server is Okay")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
